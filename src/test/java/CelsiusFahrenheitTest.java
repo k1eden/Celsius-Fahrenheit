59dfa2f74a9cdb40a929966b32a9156c45ae0a0d
k1eden <beglaryan.ga@edu.spbstu.ru>
2022-12-07 23:28:09 +0300

    #16 Module tests added

---
+++ src/test/java/CelsiusFahrenheitTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CelsiusFahrenheitTest {
+
+    private final CelsiusFahrenheit test = new CelsiusFahrenheit(40);
+
+    @Test
+    void converter() {
+        double result = 104;
+        assertEquals(test.converter(), result);
+
+        test.celsiusTemp = 0;
+        result = 32;
+
+        assertEquals(test.converter(), result);
+
+        test.celsiusTemp = -3;
+        result = 26.6;
+
+        assertEquals(test.converter(), result);
+
+    }
+}
